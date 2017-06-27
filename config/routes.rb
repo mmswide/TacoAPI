57c95dc61f666a993818c66157d4d39c554630e8
mac <mac@macs-Mac.local>
2017-06-27 22:45:37 +0200

    add main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'main_page#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount API => '/'
 end
